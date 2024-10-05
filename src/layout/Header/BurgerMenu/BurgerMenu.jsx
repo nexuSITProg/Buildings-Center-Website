288e8d592beab2bf32a256d7f55212d61097f93f
Max <nexusitproger@gmail.com>
2024-10-06 01:27:37 +0500

    adaptation start

---
+++ src/layout/Header/BurgerMenu/BurgerMenu.jsx
@@ -0,0 +1,11 @@
+import './BurgerMenu.style.css';
+
+export const BurgerMenu = () => {
+  return (
+    <div className='burger-menu'>
+      <div className="burger-menu__line"></div>
+      <div className="burger-menu__line"></div>
+      <div className="burger-menu__line"></div>
+    </div>
+  )
+}
--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,15 +1,23 @@
-import { Logo } from '@components/Logo/Logo'
-import { Menu } from '@layout/Header/Menu/Menu'
-import { Contacts } from '@layout/Header/Contacts/Contacts'
-import { SocialMedia } from '@components/SocialMedia/SocialMedia'
-import { MainButton } from '@components/Buttons/MainButton/MainButton'
+import { useMediaQuery } from 'react-responsive';
+import { BurgerMenu } from './BurgerMenu/BurgerMenu';
+import { Logo } from '@components/Logo/Logo';
+import { Menu } from '@layout/Header/Menu/Menu';
+import { Contacts } from '@layout/Header/Contacts/Contacts';
+import { SocialMedia } from '@components/SocialMedia/SocialMedia';
+import { MainButton } from '@components/Buttons/MainButton/MainButton';
 
 import './Header.style.css';
 
 export const Header = () => {
+  const isTablet = useMediaQuery({query: '(max-width:1440px)'});
+
   return (
     <header className="header">
       <Logo />
+      {isTablet ? 
+      <BurgerMenu />
+      : 
+      <>
       <Menu />
       <Contacts 
         phone={'8 800 000 00 00'}
@@ -18,6 +26,7 @@ export const Header = () => {
       <MainButton 
         content={'Перезвони мне'}
       />
+      </>}
     </header>
   )
 }
