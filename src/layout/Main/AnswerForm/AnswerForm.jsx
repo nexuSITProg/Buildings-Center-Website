c3a86f9a8b4a16df9429addc7b2b3885e050a8e7
Max <nexusitproger@gmail.com>
2024-10-12 12:12:56 +0500

    Answer form created

---
+++ src/layout/Main/AnswerForm/AnswerForm.jsx
@@ -0,0 +1,20 @@
+import { MainButton } from '@components/Buttons/MainButton/MainButton';
+
+import './AnswerForm.style.css';
+
+export const AnswerForm = () => {
+  return (
+    <form className='main__answer-form'>
+        <h1 className="main__answer-form__header">Мы готовы ответить на все Ваши вопросы</h1>
+        <div className="main__answer-form__inputs">
+            <input className='main__answer-form__input' type='text' name="" id="" placeholder='Имя'/>
+            <input className='main__answer-form__input' type="phone" name="" id="" placeholder='+7 (***) ***-**-**'/>
+            <MainButton 
+                content={'Отправить'}
+            />
+            <p className='main__answer-form__text'>Нажимая кнопку «Отправить», вы подтверждаете, что согласны на обработку персональных данных.</p>
+        </div>
+        <img className='main__answer-form__img' src="src/assets/question-icon.svg" alt="question" />
+    </form>
+  )
+}
