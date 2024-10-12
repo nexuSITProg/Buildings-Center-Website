eff9825a6ecd8fe6510e3681ab52c4e200c54ef6
Max <nexusitproger@gmail.com>
2024-10-13 02:53:19 +0500

    GH pages configure

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/[Buildings-Center-Website]/",
   plugins: [react()],
   resolve: {
     alias: [
